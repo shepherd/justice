20086e90c2387eb5fdfd4c844435af46dcbd4e5d
sheriffderek <admin@sheriffderek.consulting>
2016-09-25 14:27:53 -0700

    Loose stopping point before hooking the sliders back up.

--- app/components/record-indicator.js
+++ app/components/record-indicator.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['record-indicator']
+  classNames: ['record-indicator'],
+  didInsertElement() {
+    Ember.$('body').find('.record-indicator').css('opacity', 0);
+  },
+  didRender() {
+    Ember.run.later( function() {
+      Ember.$('body').find('.record-indicator').animate({
+        'opacity': 1
+      }, 500);
+    }, 2000);
+  }
 });
