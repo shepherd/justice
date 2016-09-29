96162351bbd4be0fb348ed48826573a001d558da
Seth Brasile <seth.brasile@gmail.com>
2016-09-29 14:03:53 -0500

    removed audiocontext-polyfill
    this polyfill doesn’t actually bring audiocontext up to spec, it only
    maps AudioContext to webkitAudioContext, which is easy to do without a
    dep :D

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,8 +7,6 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('bower_components/audiocontext-polyfill/audiocontext-polyfill.js');
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
