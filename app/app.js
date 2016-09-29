a4e74761d3a11f001964bf1971bec946be026fda
Seth Brasile <seth.brasile@gmail.com>
2016-09-29 14:04:39 -0500

    "polyfill" for AudioContext

--- app/app.js
+++ app/app.js
@@ -7,6 +7,12 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+// window.AudioContext does not exist in safari, must use
+// window.webkitAudioContext instead
+if (!window.AudioContext && window.webkitAudioContext) {
+  window.AudioContext = window.webkitAudioContext;
+}
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
