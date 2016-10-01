842d8d5f141f151281ae92e41242cd4321b50862
sheriffderek <admin@sheriffderek.consulting>
2016-09-30 18:45:53 -0700

    Updates storyData

--- app/storyData.js
+++ app/storyData.js
@@ -14,13 +14,14 @@ const storyData = [
       "step": 5,
     },
     "sentence": {
+      "headline": "Father of 7, facing 13+ years for minor, non-violent crime.",
       "time": "13.3 years",
       "shortTime": "13.3",
       "story": "In New Orleans, a father of seven and local restaurateur was stopped by police over by police while riding his bicycle in 2011. The officers searched the man and found 2.8 grams of marijuana, the equivalent of two joints.",
-      "reasoning": "Because the man had prior non-violent drug offenses -- for small amounts of marijuana -- a New Orleans jury convicted him under a Louisiana state law that doles out harsher punishments for “habitual offenders.” In fact, the sentencing range for the third offense of possession of marijuana is up to 20 years in prison. Currently, the man does not face the possibility of parole."
+      "reasoning": "Because the man had prior non-violent drug offenses – for small amounts of marijuana – a New Orleans jury convicted him under a Louisiana state law that doles out harsher punishments for “habitual offenders.” In fact, the sentencing range for the third offense of possession of marijuana is up to 20 years in prison. Currently, the man does not face the possibility of parole."
     },
     "effects": {
-      "title": "?",
+      "title": "Extreme sentencing affects our communities and broader societal welfare.",
       "facts": [
         {
           "id": 1,
@@ -49,7 +50,7 @@ const storyData = [
     "location": "Texas",
     "charge": {
       "description": "Facilitating one crack cocaine transaction in 1999.",
-      "overview": "A 32-year-old single mother with no previous convictions was found guilty of one count of conspiracy to distribute crack cocaine, and acquitted of six counts of possession of crack cocaine and aiding and abetting",
+      "overview": "A 32-year-old single mother with no previous convictions was found guilty of one count of conspiracy to distribute crack cocaine, and acquitted of six counts of possession of crack cocaine and aiding and abetting.",
       "priors": "",
       "prompt": "",
       "min": 0,
@@ -59,24 +60,25 @@ const storyData = [
       "step": 1,
     },
     "sentence": {
+      "headline": "Single mother sentenced to life in prison with no physical evidence.",
       "time": "Life without the possibility of parole.",
       "shortTime": "Life",
       "story": "The young woman in question grew up in poverty in the small rural town of Terrell, Texas.  Her mother was paraplegic, and from a young age the woman was forced to take care of her siblings.  In this case, prosecutors claimed the young woman served as a drug courier for a drug supplier in Houston.",
       "reasoning": "In 1999, the woman was arrested as part of a drug task force operation, where all 105 people who were arrested were black.  During trial, no physical evidence was ever presented by investigators. However, because of Texas sentencing laws, the judge increased the punishment because the amount of theoretical crack cocaine (30 kg) being trafficked. The woman is not eligible for sentence reductions."
     },
     "effects": {
-      "title": "?",
+      "title": "The staggering growth and cost of drug-related incarcerations",
       "facts": [
         {
           "id": 1,
           "number": "50",
-          "visuaNumber": "5%",           
+          "visuaNumber": "50%",           
           "copy": "As of 2014, half of all federal inmates are incarcerated on drug-related convictions."
         },
         {
           "id": 2,
           "number": "16",
-          "visuaNumber": "1",           
+          "visuaNumber": "16%",           
           "copy": "In 1970, just 16 percent of federal inmates were locked up on drug-related convictions."
         },
         {
@@ -94,7 +96,7 @@ const storyData = [
     "location": "Florida",
     "charge": {
       "description": "Conspiracy and possession with intent to distribute LSD.",
-      "overview": "A 24-year-old Vegan “Deadhead” is charged with selling LSD Grateful Dead concert.",
+      "overview": "A 24-year-old Vegan “Deadhead” is charged with selling LSD at Grateful Dead concert.",
       "priors": "Two prior convictions for the possession of small amounts of marijuana and LSD. No arrest history for violent crimes.",
       "prompt": "",
       "min": 0,
@@ -104,13 +106,14 @@ const storyData = [
       "step": 1,
     },
     "sentence": {
+      "headline": "Life in prison, solitary confinement for third-strike offense.",
       "time": "Life without the possibility of parole.",
       "shortTime": "Life",
       "story": "In 1992, a young man touring traveling with the Grateful Dead on a musical tour sold LSD to a friend who had become a confidential informant for police officers. Over the course of 60 days, the man dealt LSD in five transactions, including through the U.S. postal service.",
       "reasoning": "In this case, the judge included the weight of the paper that carried the LSD when factoring the man’s sentence. That brought the total weight of the LSD above 10 grams, triggering a mandatory minimum sentence of life in prison for “third-strike” offenders. Like many other states, Florida has a “habitual offender” law which allows for life without parole sentences, even for nonviolent crimes. The man has now served over 23 years in prison, with much of that time in solitary confinement."
     },
     "effects": {
-      "title": "?",
+      "title": "Too many Americans are trapped by the injustices of our system.",
       "facts": [
         {
           "id": 1,
@@ -140,7 +143,7 @@ const storyData = [
     "charge": {
       "description": "Growing about three dozen marijuana plants.",
       "overview": "A 75-year-old disabled veteran is convicted with growing three dozen marijuana plants behind his son’s home in Dothan, Alabama. He says he intended to use the plants to alleviate chronic pain, which prosecutors did not dispute.",
-      "priors": "Three prior convictions a string of robberies committed two decades ago, for which the man served 10 years in prison.",
+      "priors": "Three prior convictions for a string of robberies committed two decades ago, for which the man served 10 years in prison.",
       "prompt": "",
       "min": 0,
       "specialMin": "",
@@ -149,13 +152,14 @@ const storyData = [
       "step": 1,
     },
     "sentence": {
-      "time": "",
-      "shortTime": "",
+      "headline": "Life in prison, no parole for 75-year-old growing marijuana.",
+      "time": "Life without the possibility of parole.",
+      "shortTime": "Life",
       "story": "In this case, an elderly man was charged and convicted for growing a total of 34 marijuana plants. He was growing the plants to alleviate chronic pain (and medicinal marijuana is not legal in Alabama). Because the total weight of the plant, including the unusable stalks and stems weighed 2.85 pounds, the conviction triggered a threshold for a more serious offense on anyone in possession of more than 2.2 pounds of marijuana.",
       "reasoning": "Alabama, too, is one of four states that mandates a life without parole sentence for simple possession of small amounts of marijuana by people with certain prior felony convictions."
     },
     "effects": {
-      "title": "?",
+      "title": "The cycle of injustice is taking its toll.",
       "facts": [
         {
           "id": 1,
@@ -172,7 +176,7 @@ const storyData = [
         {
           "id": 3,
           "number": "27.6",
-          "visuaNumber":"2.7%6",           
+          "visuaNumber":"27.6%",           
           "copy": "More than a quarter of drug offenders were incarcerated because of marijuana-related convictions."
         }
       ]
@@ -180,7 +184,7 @@ const storyData = [
   },
   {
     "id": 5,
-    "name": "",
+    "name": "Truck leasing",
     "location": "Georgia",
     "charge": {
       "description": "Leasing trucks to marijuana smugglers in Georgia.",
@@ -194,18 +198,19 @@ const storyData = [
       "step": 1,
     },
     "sentence": {
+      "headline": "Life in prison for “conspiracy to distribute.”",
       "time": "Life without the possibility of parole.",
       "shortTime": "Life",
-      "story": "In this case, the man -- a successful business owner and truck mechanic --  allowed  a group of marijuana drug smugglers to use his fleet of trucks to carry drugs within his home state of Georgia.",
-      "reasoning": "This was no simple marijuana possession charge: Because federal investigators claimed the man had direct knowledge of the drug enterprise, they charged him with “Conspiracy to Distribute” the total amount of marijuana being trafficked -- which exceeded 1,000 kilograms -- and which amplified the length of the sentence. Meanwhile, the drivers from the trucking company who cooperated with federal agents (and implicated the man) never spent a day in prison."
+      "story": "In this case, the man – a successful business owner and truck mechanic –  allowed  a group of marijuana drug smugglers to use his fleet of trucks to carry drugs within his home state of Georgia.",
+      "reasoning": "This was no simple marijuana possession charge: Because federal investigators claimed the man had direct knowledge of the drug enterprise, they charged him with “Conspiracy to Distribute” the total amount of marijuana being trafficked – which exceeded 1,000 kilograms – and which amplified the length of the sentence. Meanwhile, the drivers from the trucking company who cooperated with federal agents (and implicated the man) never spent a day in prison."
     },
     "effects": {
-      "title": "?",
+      "title": "The disproportionate impact and effects of our current policies.",
       "facts": [
         {
           "id": 1,
           "number": "65",
-          "visuaNumber": "6",           
+          "visuaNumber": "65%",           
           "copy": "More than six out of ten of those convicted of second or third strikes are serving time in prison for nonviolent crimes."
         },
         {
@@ -222,52 +227,7 @@ const storyData = [
         }
       ]
     }
-  },
-  {
-    "id": 6,
-    "name": "",
-    "location": "",
-    "charge": {
-      "description": "",
-      "overview": "",
-      "priors": "",
-      "prompt": "",
-      "min": 0,
-      "specialMin": "",
-      "max": 100,
-      "specialMax": "",
-      "step": 1,
-    },
-    "sentence": {
-      "time": "",
-      "shortTime": "",
-      "story": "",
-      "reasoning": ""
-    },
-    "effects": {
-      "title": "?",
-      "facts": [
-        {
-          "id": 1,
-          "number": "",
-          "visuaNumber": "",           
-          "copy": ""
-        },
-        {
-          "id": 2,
-          "number": "",
-          "visuaNumber": "",           
-          "copy": ""
-        },
-        {
-          "id": 3,
-          "number": "",
-          "visuaNumber": "",           
-          "copy": ""
-        }
-      ]
-    }
-  },
+  }
 ];
 
 export default storyData;
