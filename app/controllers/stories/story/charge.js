60c505822eddf1f6221d56dc7b04cfda331859e1
Seth Brasile <seth.brasile@gmail.com>
2016-10-01 16:35:30 -0500

    use participant-state service to track user's choices

--- app/controllers/stories/story/charge.js
+++ app/controllers/stories/story/charge.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  participantState: Ember.inject.service(),
+
+  selectedSentence: Ember.computed('model.story.id', 'participantState.values.[]', function() {
+    const currentStoryId = this.get('model.story.id');
+    return this.get('participantState').getValueFor(currentStoryId);
+  }),
+
   valueSelected: Ember.computed('model.selectedSentence', function() {
     const value = this.get('model.selectedSentence');
     const min = this.get('model.story.charge.min');
@@ -13,9 +20,21 @@ export default Ember.Controller.extend({
   }),
 
   actions: {
+    selectSentence(value) {
+      const currentStoryId = this.get('model.story.id');
+      this.get('participantState').pushValue(currentStoryId, value);
+    },
+
     transitionToSentence() {
       const currentStoryId = this.get('model.story.id');
+      const participantState = this.get('participantState');
       // do awesome stuff ? and .then(
+
+      // if value has not been set by user, use default which is set by slider
+      if (participantState.getValueFor(currentStoryId) === undefined) {
+        participantState.pushValue(currentStoryId, this.get('selectedSentence'));
+      }
+
       this.transitionToRoute('stories.story.sentence', currentStoryId);
     }
   }
--- app/controllers/stories/story/sentence.js
+++ app/controllers/stories/story/sentence.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  participantState: Ember.inject.service(),
+
+  selectedSentence: Ember.computed('model.story.id', 'participantState.values.[]', function() {
+    const currentStoryId = this.get('model.story.id');
+    return this.get('participantState').getValueFor(currentStoryId);
+  }),
+
   actions: {
     transitionToEffects() {
       const currentStoryId = this.get('model.story.id');
