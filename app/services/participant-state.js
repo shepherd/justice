e7054c4caa3ff5a7384a5dd567b6e6847f112a82
Seth Brasile <seth.brasile@gmail.com>
2016-10-01 14:04:23 -0500

    store values in participant-state service in an array
    instead of on the service itself

--- app/services/participant-state.js
+++ app/services/participant-state.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  values: Ember.A(),
+
+  pushValue(id, value) {
+    this.get('values').pushObject({ id, value });
+  },
+
+  getValueFor(id) {
+    const requestedObject = this.get('values').findBy('id', id);
+
+    if (requestedObject && requestedObject.value) {
+      return requestedObject.value;
+    }
+  },
+
   clearState() {
-    const keys = Object.keys(this);
-    keys.map((key) => this.set(key, undefined));
+    this.set('values', Ember.A());
   }
 });
--- tests/unit/services/participant-state-test.js
+++ tests/unit/services/participant-state-test.js
@@ -10,11 +10,23 @@ test('it exists', function(assert) {
   assert.ok(service);
 });
 
-test('it has a "clearState" method that removes all stored state', function(assert) {
+test('it has a "pushValue" method that pushes a POJO into the "values" array', function(assert) {
   let service = this.subject();
-  service.set('blah', 5);
+  service.pushValue(1, 55);
+  assert.deepEqual(service.get('values')[0], { id: 1, value: 55 });
+});
 
-  assert.ok(service.get('blah'));
+test('it has a "getValueFor" method that gets the "value" key from a given POJO from the "values" array', function(assert) {
+  let service = this.subject();
+  service.get('values').pushObject({ id: 1, value: 55 });
+  assert.deepEqual(service.getValueFor(1), 55);
+});
+
+test('it has a "clearState" method that removes all stored values from the "values" array', function(assert) {
+  let service = this.subject();
+  service.pushValue(1, 55);
+
+  assert.equal(service.getValueFor(1), 55);
   service.clearState();
-  assert.notOk(service.get('blah'));
+  assert.notOk(service.getValueFor(1));
 });
