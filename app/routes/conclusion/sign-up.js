61774149f000e0a32da0981b59cf70738f16cca0
sheriffderek <admin@sheriffderek.consulting>
2016-10-02 11:27:52 -0700

    Adds hook for email-submit

--- app/routes/conclusion/sign-up.js
+++ app/routes/conclusion/sign-up.js
@@ -4,6 +4,18 @@ export default Ember.Route.extend({
   actions: {
     transitionToThankYou() {
       this.transitionTo('conclusion.thank-you');
+    },
+    emberChimpDidSubmit(promise) {
+      promise
+      .then(response => {
+        if (response.result === 'success') {  
+          console.log("Ember Chimp submitted Successfully!");
+          // transition to "conclusion.thank-you"
+        } else {
+          console.log("Ember Chimp error message:" + response.msg);
+        }
+      })
+      .catch(error => console.log("Ember Chimp had an Ajax Error."));
     }
   }
 });
