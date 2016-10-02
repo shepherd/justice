a0d60362d68cfc58554b36296e3bd3abdd3a1332
sheriffderek <admin@sheriffderek.consulting>
2016-10-01 18:45:35 -0700

    Adds class on route to app

--- app/app.js
+++ app/app.js
@@ -19,6 +19,23 @@ App = Ember.Application.extend({
   Resolver
 });
 
+Ember.Route.reopen({
+  activate: function() {
+    var cssClass = this.toCssClass();
+    // you probably don't need the application class
+    // to be added to the body
+    if (cssClass != 'application') {
+      Ember.$('body').addClass(cssClass);
+    }
+  },
+  deactivate: function() {
+    Ember.$('body').removeClass(this.toCssClass());
+  },
+  toCssClass: function() {
+    return this.routeName.replace(/\./g, '-').dasherize();
+  }
+});
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
