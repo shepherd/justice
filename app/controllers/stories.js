ea6f4ec82247d229bb13ac97b50ec35af892a4b3
sheriffderek <admin@sheriffderek.consulting>
2016-10-02 14:02:40 -0700

    Ammends transitionTo vs transitionToRoute

--- app/controllers/stories.js
+++ app/controllers/stories.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     returnHome() {
-      this.transitionTo('application');
+      this.transitionToRoute('application');
     }
   }
 });
