24fc336d9cc88a83acd25acbbdc923a292040074
sheriffderek <admin@sheriffderek.consulting>
2016-10-01 20:58:10 -0700

    Place index page squares for later

--- app/router.js
+++ app/router.js
@@ -8,10 +8,10 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('title', { path: '/' }, function() {
+    this.route('welcome');
     this.route('overview');
     this.route('how-to');
     this.route('why');
-    this.route('welcome');
   });
   this.route('stories', function() {
     this.route('story', { path: ':story_id' }, function() {
