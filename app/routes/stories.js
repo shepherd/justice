5df87f9a948b22a21f676984d49e71a955bc53b0
sheriffderek <admin@sheriffderek.consulting>
2016-10-02 22:33:30 -0700

    Changes transition to title?

--- app/routes/stories.js
+++ app/routes/stories.js
@@ -13,7 +13,7 @@ export default Ember.Route.extend(RemoveBackgroundImage, {
   actions: {
     transitionToTitle() {
       this.get('participantState').clearState();
-      this.transitionTo('title');
+      this.transitionTo('application');
     }
   }
 });
