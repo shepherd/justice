6d865138b4100c80e9532d9bf49b107821e2ba16
Seth Brasile <seth.brasile@gmail.com>
2016-10-02 23:34:49 -0500

    replace submit action with 'play again' button

--- app/routes/conclusion/sign-up.js
+++ app/routes/conclusion/sign-up.js
@@ -7,22 +7,6 @@ export default Ember.Route.extend({
     transitionToTitle() {
       this.get('participantState').clearState();
       this.transitionTo('title');
-    },
-    emberChimpDidSubmit(promise) {
-      promise
-      .then(response => {
-        // This stuff doesn't work, it errors even when the request succeeds
-        // if (response.result === 'success') {
-          // console.log("Ember Chimp submitted Successfully!");
-          // this.send('transitionToTitle');
-        // } else {
-          // console.log("Ember Chimp error message:" + response.msg);
-        // }
-      // })
-      // .catch(error => console.log("Ember Chimp had an Ajax Error."));
-        Ember.run.later(() => this.send('transitionToTitle'), 5000);
-      })
-      .catch(() => Ember.run.later(() => this.send('transitionToTitle'), 5000));
     }
   }
 });
