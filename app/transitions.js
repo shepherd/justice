66aad892f8e933265e8449debcc40f4d28c18a4d
sheriffderek <admin@sheriffderek.consulting>
2016-10-03 01:29:53 -0700

    removes old transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,41 +1,9 @@
 
 export default function() {
 
-  // titles
-
-  // this.transition(
-  //   this.fromRoute('stories.story.charge'),
-  //   this.toRoute('stories.story.sentence'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
-
-  // this.transition(
-  //   this.fromRoute('stories.story.sentence'),
-  //   this.toRoute('stories.story.effect'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
-
-  // this.transition(
-  //   this.fromRoute('stories.story.effect'),
-  //   this.toRoute('stories.story.charge'),
-  //   this.use('toUp'),
-  //   this.reverse('toDown')
-  // );
-
-  // story data
-  this.transition(
-    this.hasClass('story-headers'),
-    this.toValue(true),
-    this.use('fade', { duration: 1000 }),
-  );
-
-  // conclusion
-
   // default?
-  this.transition( // default transition for all views
-    this.use('crossFade'),
+  this.transition(
+    this.use('crossFade')
   );
 
 }
